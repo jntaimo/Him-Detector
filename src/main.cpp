df8611bb6a4ff1849bcb12b647a56b3476071764
jntaimo <jntaimo@gmail.com>
2022-12-17 18:14:38 -0500

    Added boosted graphics for analysis

--- src/main.cpp
+++ src/main.cpp
@@ -23,7 +23,7 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 Adafruit_ADXL343 accel = Adafruit_ADXL343(12345);
 float accelMag = 9;
 float filtAccelMag = 9;
-float alpha = 0.05;
+float alpha = 0.01;
 float accelMoveThreshold = 10;
 long accelReadDelay = 10; //milliseconds
 long accelPrintDelay = 100;
@@ -37,6 +37,10 @@ enum mode {
 
 //default the current status to chill
 mode currentStatus = CHILL;
+void testdrawrect(void);
+void testdrawcircle(void);
+void scroll(void);
+void testdrawline(void);
 
 void accelerometerSetup() {
 
@@ -61,14 +65,26 @@ void vibrationSetup(){
   // default, internal trigger when sending GO command
   drv.setMode(DRV2605_MODE_INTTRIG); 
 }
+
+void displaySetup(){
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    while(1) delay(10); // Don't proceed, loop forever
+  }
+  display.display();
+  display.setTextColor(SSD1306_WHITE);
+}
 void setup(void)
 {
   Serial.begin(115200);
-  while (!Serial);
+  //while (!Serial);
   accelerometerSetup();
   vibrationSetup();
+  displaySetup();
 }
 
+long lastPrint = 0;
+long printDelay = 250;
 
 void loop(void)
 {
@@ -88,20 +104,152 @@ void loop(void)
 
   switch(currentStatus){
       case CHILL:
-        Serial.println("Chillin Like a Villian");
-
+        if (millis() - lastPrint > printDelay){
+          Serial.println("Chillin Like a Villian");
+          lastPrint = millis();
+        }   
         break;
       case HIMOTHY:
-        
         Serial.println("You are he");
-          // set the effect to play
-        drv.setWaveform(0, VIBRATION_EFFECT);  // play effect 
-        drv.setWaveform(1, 0);       // end waveform
-        // play the effect!
-        drv.go();
-        delay(500);
+        display.clearDisplay();
+        display.setCursor(0,0);
+        display.setTextSize(2);
+        display.println("Analysis...");
+        display.println("Analysis...");
+        display.println("Analysis...");
+        display.display();
+        scroll();
+        delay(1000);
+        display.clearDisplay();
+        display.display();
+        testdrawrect();
+        testdrawcircle();
+        testdrawline();
+        display.clearDisplay();
+        display.setCursor(10,0);
+        display.setTextSize(6);
+
+        display.println(F("HIM"));
+        display.setTextSize(2);
+        display.println(F(" DETECTED!"));
+        display.display();
+
+        for (int i = 0; i < 8;i++ ){
+          drv.setWaveform(0, VIBRATION_EFFECT);  // play effect 
+          drv.setWaveform(1, 0);       // end waveform   // end waveform
+          drv.go();
+
+          
+          display.invertDisplay(i%2);
+          delay(300);
+        }
+        
+        //revert back to a normal display
+        display.invertDisplay(false);
+        display.clearDisplay();
+        display.setCursor(0,0);
+        display.setTextSize(2);
+        display.println(F("Waiting \nfor \nHIM"));
+        display.display();
+        delay(100);
+        lastPrint = millis();
+        //reset acceleratio mag so it can't be triggered for a bit
+        filtAccelMag = 0;
         currentStatus = CHILL;
         break;
   }
+}
 
+void testdrawrect(void) {
+  //display.clearDisplay();
+
+  for(int16_t i=0; i<display.height()/2; i+=2) {
+    display.drawRect(i, i, display.width()-2*i, display.height()-2*i, SSD1306_WHITE);
+    display.display(); // Update screen with each newly-drawn rectangle
+    delay(5);
+  }
+
+  delay(500);
+}
+void testdrawcircle(void) {
+  //display.clearDisplay();
+
+  for(int16_t i=0; i<max(display.width(),display.height())/2; i+=2) {
+    display.drawCircle(display.width()/2, display.height()/2, i, SSD1306_WHITE);
+    display.display();
+    delay(5);
+  }
+
+  delay(500);
+}
+
+void scroll(void){
+  display.startscrollright(0x00, 0x0F);
+  delay(2000);
+  display.stopscroll();
+  delay(100);
+  display.startscrollleft(0x00, 0x0F);
+  delay(2000);
+  display.stopscroll();
+}
+
+void testdrawline(void) {
+  int16_t i;
+
+  display.clearDisplay(); // Clear display buffer
+
+  for(i=0; i<display.width(); i+=4) {
+    display.drawLine(0, 0, i, display.height()-1, SSD1306_WHITE);
+    display.display(); // Update screen with each newly-drawn line
+    delay(1);
+  }
+  for(i=0; i<display.height(); i+=4) {
+    display.drawLine(0, 0, display.width()-1, i, SSD1306_WHITE);
+    display.display();
+    delay(1);
+  }
+  delay(150);
+
+  display.clearDisplay();
+
+  for(i=0; i<display.width(); i+=4) {
+    display.drawLine(0, display.height()-1, i, 0, SSD1306_WHITE);
+    display.display();
+    delay(1);
+  }
+  for(i=display.height()-1; i>=0; i-=4) {
+    display.drawLine(0, display.height()-1, display.width()-1, i, SSD1306_WHITE);
+    display.display();
+    delay(1);
+  }
+  delay(150);
+
+  display.clearDisplay();
+
+  for(i=display.width()-1; i>=0; i-=4) {
+    display.drawLine(display.width()-1, display.height()-1, i, 0, SSD1306_WHITE);
+    display.display();
+    delay(1);
+  }
+  for(i=display.height()-1; i>=0; i-=4) {
+    display.drawLine(display.width()-1, display.height()-1, 0, i, SSD1306_WHITE);
+    display.display();
+    delay(1);
+  }
+  delay(150);
+
+  display.clearDisplay();
+
+  for(i=0; i<display.height(); i+=4) {
+    display.drawLine(display.width()-1, 0, 0, i, SSD1306_WHITE);
+    display.display();
+    delay(1);
+  }
+  for(i=0; i<display.width(); i+=4) {
+    display.drawLine(display.width()-1, 0, i, display.height()-1, SSD1306_WHITE);
+    display.display();
+    delay(1);
+  }
+
+  delay(2000); // Pause for 2 seconds
 }
