143ebf5f87a51b9cb0569070cd31e9060eb43eb9
jntaimo <jntaimo@gmail.com>
2022-12-17 17:08:20 -0500

    Basic vibration added

--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,24 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_ADXL343.h>
 #include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include "Adafruit_DRV2605.h"
+//VIBRATION START
+Adafruit_DRV2605 drv;
+#define VIBRATION_EFFECT 52
+//VIBRATION STOP
+//DISPLAY START
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3D ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+//DISPLAY STOP
 
+//ACCELEROMETER START
 /* Assign a unique ID to this sensor at the same time */
 /* Uncomment following line for default Wire bus      */
 Adafruit_ADXL343 accel = Adafruit_ADXL343(12345);
-enum mode {
-  HIMOTHY,
-  CHILL
-};
-
 float accelMag = 9;
 float filtAccelMag = 9;
 float alpha = 0.05;
@@ -19,14 +28,17 @@ float accelMoveThreshold = 10;
 long accelReadDelay = 10; //milliseconds
 long accelPrintDelay = 100;
 long lastAccelRead = 0;
+//ACCELEROMETER START
+
+enum mode {
+  HIMOTHY,
+  CHILL
+};
 
 //default the current status to chill
-  mode currentStatus = CHILL;
-void setup(void)
-{
-  Serial.begin(115200);
-  while (!Serial);
-  Serial.println("Accelerometer Test"); Serial.println("");
+mode currentStatus = CHILL;
+
+void accelerometerSetup() {
 
   /* Initialise the sensor */
   if(!accel.begin())
@@ -35,9 +47,26 @@ void setup(void)
     Serial.println("Ooops, no ADXL343 detected ... Check your wiring!");
     while(1);
   }
-  
   accel.setRange(ADXL343_RANGE_8_G);
-  
+
+}
+
+void vibrationSetup(){
+  if (! drv.begin()) {
+    Serial.println("Could not find DRV2605");
+    while (1) delay(10);
+  }
+  drv.selectLibrary(1);
+  // I2C trigger by sending 'go' command 
+  // default, internal trigger when sending GO command
+  drv.setMode(DRV2605_MODE_INTTRIG); 
+}
+void setup(void)
+{
+  Serial.begin(115200);
+  while (!Serial);
+  accelerometerSetup();
+  vibrationSetup();
 }
 
 
@@ -63,7 +92,14 @@ void loop(void)
 
         break;
       case HIMOTHY:
+        
         Serial.println("You are he");
+          // set the effect to play
+        drv.setWaveform(0, VIBRATION_EFFECT);  // play effect 
+        drv.setWaveform(1, 0);       // end waveform
+        // play the effect!
+        drv.go();
+        delay(500);
         currentStatus = CHILL;
         break;
   }
